d35cc0bdafe792d43dc840d29060dd539caaae8c
Sanya's Mac <sanya15arora@gmail.com>
2025-10-29 19:39:42 +0530

    ui fix

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -20,19 +20,20 @@ const Portfolio = () => {
                 <SectionHeading firstTitle={AppText.Creative} secondTitle={AppText.Portfolio}/>
                 <img src={robo} className="w-[70px] ml-5 animate-bounce" alt={'portfolio heading image'}/>
             </div>
-            <div className='grid grid-cols-1 items-center content-center md:grid-cols-2 xl:grid-cols-3
-        md:px-24 lg:px-48 p-6 '>
+            <div className='grid grid-cols-1 items-center  md:grid-cols-2 xl:grid-cols-3
+        md:px-24 lg:px-48 p-6 gap-2'>
                 {portfolioList.map((project, index) => (
                     <div key={index} className='p-2 flex flex-col m-2 rounded-lg bg-[#f6cc7740] md:h-[460px] xl:h-[400px]
                 transition-all ease-in-out group hover:scale-110'>
                         <a href={project.demoLink}
                            target="_blank"
-                           rel="noopener noreferrer">
+                           rel="noopener noreferrer"
+                           className={"grid justify-items-center"}>
                             <img src={project.image} className="h-[180px] object-fit rounded-lg"
                                  alt={'project image'}/>
                             <h1 className='text-[14px] group-hover:scale-110 m-2 text-center  font-bold'>{project.title}</h1>
 
-                            <div className={'flex flex-wrap lg:justify-center'}>
+                            <div className={'flex flex-wrap justify-center'}>
                                 {project.technologies.map((tech, index) =>
                                         <span key={index}
                                               className={'m-1 rounded bg-[#ffd23f] px-2 py-1 text-sm font-medium text-white'}>
